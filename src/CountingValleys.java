0e96cfb4f575c36e9a7a6ed08a53b39fb0e249e3
Abora <https://github.com/abora97>
2020-05-14 01:45:04 +0200

    CountingValleys

---
+++ src/CountingValleys.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class CountingValleys {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        String terrain = input.nextLine();
+        int level = 0; //Start at sea level
+        int valleys = 0;
+        boolean belowSea = false;
+
+        for (int i = 0; i < n; i++) {
+            char slope = terrain.charAt(i);
+            if (slope == 'U')//Uphill
+                level++;
+            else//Downhill
+                level--;
+
+            //Handle transitions
+            if (!belowSea && level < 0) {
+                valleys++;
+                belowSea = true;
+            }
+
+            if (level >= 0)//We are back above sea level
+                belowSea = false;
+        }
+        System.out.println(valleys);
+
+
+    }
+}
