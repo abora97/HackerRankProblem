c87ed98de4fe51fe4d31ecf72ed372876f055c0c
Mahmoud Ahmed Elebiary <mahmoud.elebiary@yahoo.com>
2021-02-07 22:33:13 +0200

    solve some problem

--- src/CompetitiveGaming.java
+++ src/CompetitiveGaming.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
-public class competitiveGaming {
+public class CompetitiveGaming {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int k = input.nextInt();
+
+        int n = input.nextInt();
+
+        int rank = k;
+
+        Integer[] arr = new Integer[n];
+        Integer[] arrFinal = new Integer[n];
+        Integer[] arrDuplicate = new Integer[n];
+
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i].equals(arr[j]))
+                    arrDuplicate[i] = arr[j];
+
+            }
+        }
+
+        System.out.println("                   " + rank);
+
+        for (int i = 0; i < k; i++) {
+            arrFinal[i] = arr[i];
+        }
+
+        for (int i = 0; i < arrFinal.length; i++) {
+
+            for (int j = 0; j < arrDuplicate.length; j++) {
+                if (arrFinal != null) {
+                    if (arrFinal[i].equals(arrDuplicate[j])) {
+                        rank++;
+
+                    }
+                }
+            }
+
+        }
+
+        System.out.println(rank);
+
+
+    }
 }
--- src/LeftRotation.java
+++ src/LeftRotation.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LeftRotation {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        int arrSize = input.nextInt();
+
+        //rotationCount determine the number of times an array should be rotated
+        int rotationCount=input.nextInt();
+
+        //Initialize array
+        int [] arr =new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            arr[i]=input.nextInt();
+//            System.out.print(arr[i] + " ");
+        }
+
+        //Rotate the given array by n times toward left
+        for(int i = 0; i < rotationCount; i++){
+            int j, first;
+            //Stores the first element of the array
+            first = arr[0];
+            for(j = 0; j < arr.length-1; j++){
+                //Shift element of array by one
+                arr[j] = arr[j+1];
+            }
+            //First element of array will be added to the end
+            arr[j] = first;
+        }
+        System.out.println();
+
+        //Displays resulting array after rotation
+//        System.out.println("Array after left rotation: ");
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
